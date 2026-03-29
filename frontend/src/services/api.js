6872f808b6a66c174653627188aede6e0b189f3f
Vaijeenath Panchakshari <vnplatur@gmail.com>
2026-03-29 23:31:54 +0530

    update .env

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+
 const api = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: import.meta.env.VITE_API_URL+"/api" || "http://localhost:5000/api"
 });
 
 api.interceptors.request.use((config)=>{
